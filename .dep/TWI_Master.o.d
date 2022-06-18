deb39055b4d3a19a36557ea86e7ae32d612aa2e6
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2022-06-18 14:57:20 +0200

    check auf version 2018, ??

--- .dep/TWI_Master.o.d
+++ .dep/TWI_Master.o.d
@@ -1,88 +1,88 @@
 TWI_Master.o: TWI_Master.c \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h \
- /usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stdint.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/stdint.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/iom644p.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/iomxx4.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/delay.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/util/delay.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/util/delay_basic.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/math.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/pgmspace.h \
- /usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stddef.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/avr/eeprom.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/stdlib.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/ctype.h TWI_Master.h \
- Funktionen.h /usr/local/CrossPack-AVR-20131216/avr/include/avr/wdt.h \
- twimaster.c /usr/local/CrossPack-AVR-20131216/avr/include/compat/twi.h \
- /usr/local/CrossPack-AVR-20131216/avr/include/util/twi.h i2cmaster.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/io.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/sfr_defs.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/inttypes.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/lib/avr-gcc/9/gcc/avr/9.3.0/include/stdint.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/stdint.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/iom324.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/iomxx4.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/portpins.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/common.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/version.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/fuse.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/lock.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/delay.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/util/delay.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/util/delay_basic.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/math.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/interrupt.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/pgmspace.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/lib/avr-gcc/9/gcc/avr/9.3.0/include/stddef.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/eeprom.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/stdlib.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/ctype.h TWI_Master.h \
+ Funktionen.h /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/wdt.h \
+ twimaster.c /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/compat/twi.h \
+ /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/util/twi.h i2cmaster.h \
  lcd.h lcd.c err.c err.h adc.c adc.h slaves.c slaves.h display.c \
- display.h /usr/local/CrossPack-AVR-20131216/avr/include/string.h datum.c \
- version.c SPI_slave.c net.h rtc.c rtc.h
+ display.h /usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/string.h \
+ datum.c version.c SPI_slave.c net.h rtc.c rtc.h
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/io.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/io.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/sfr_defs.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/sfr_defs.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/inttypes.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/inttypes.h:
 
-/usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stdint.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/lib/avr-gcc/9/gcc/avr/9.3.0/include/stdint.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/stdint.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/stdint.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/iom644p.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/iom324.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/iomxx4.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/iomxx4.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/portpins.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/portpins.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/common.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/common.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/version.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/version.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/fuse.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/fuse.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/lock.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/lock.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/delay.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/delay.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/util/delay.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/util/delay.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/util/delay_basic.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/util/delay_basic.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/math.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/math.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/interrupt.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/interrupt.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/pgmspace.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/pgmspace.h:
 
-/usr/local/CrossPack-AVR-20131216/lib/gcc/avr/4.8.1/include/stddef.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/lib/avr-gcc/9/gcc/avr/9.3.0/include/stddef.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/eeprom.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/eeprom.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/stdlib.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/stdlib.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/ctype.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/ctype.h:
 
 TWI_Master.h:
 
 Funktionen.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/avr/wdt.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/avr/wdt.h:
 
 twimaster.c:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/compat/twi.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/compat/twi.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/util/twi.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/util/twi.h:
 
 i2cmaster.h:
 
@@ -106,7 +106,7 @@ display.c:
 
 display.h:
 
-/usr/local/CrossPack-AVR-20131216/avr/include/string.h:
+/usr/local/Cellar/avr-gcc@9/9.3.0_3/avr/include/string.h:
 
 datum.c:
 
